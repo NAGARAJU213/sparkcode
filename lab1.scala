9d79a241212112563686ccbdb67bfc85de4f2266
Nagaraju <nagarajunayakbhukya4@gmail.com>
2022-10-10 15:29:06 +0530

    second commit

--- lab1.scala
+++ lab1.scala
@@ -1,5 +1,7 @@
 val spark = SparkSession.builder.appName("lab01").master("local(*)").getOrCreate()
 
-val df = spark.read.format("csv").load("/home/hduser/hive/data/txns").toDF
+val df = spark.read.format("csv").option("inferschema",true).load("/home/hduser/hive/data/txns").toDF
 
 df.show()
+
+df.printSchema()
